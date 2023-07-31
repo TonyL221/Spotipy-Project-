29cad79bc1b1074d02150c9caf71f84616eaa13b
TonyL221 <83036049+TonyL221@users.noreply.github.com>
2023-07-31 16:44:19 -0700

    Update main.py
    sadjofopkafedsafadsasfadsfsadf

---
+++ main.py
@@ -0,0 +1,8 @@
+playlist = newPlayList('playlistName', 'playlistID')
+playlist.createPlaylist('enter name: ', 'description: ')
+playlist.copyFromPlaylist('0YCQ3PEOC3qUeuoZz2yt8k')
+playlist.organizeByYear() 
+
+#def organizeSadToHappy():
+ #   for track in sp.playlist_tracks(self.playListID)["items"]:
+    
